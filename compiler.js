73965dfb70e6f02f9f86cfd5c6cdab4623c7350c
Junee <juneekim7@gmail.com>
2023-11-11 06:15:59 +0900

    validate

--- compiler.js
+++ compiler.js
@@ -1,20 +1,17 @@
-function validate(file) {
+function isValidate(file) {
     const chars = '+1I|l=-/(){}_!\n\r '.split('')
     const check = chars.reduce((acc, cur) => {
         return acc.replace(new RegExp(`[${cur}]`, 'g'), '')
     }, file)
     if (check !== '') {
-        return {
-            success: false,
-            msg: `코드에 ${chars.join(', ')}만 포함되어야 합니다.\n 현재 코드에는 ${check}가 포함되어 있습니다.`
-        }
+        throw new Error(`코드에 ${chars.join(', ')}만 포함되어야 합니다.\n 현재 코드에는 ${check}가 포함되어 있습니다.`)
+        return false
     }
-    return {
-        success: true
-    }
+    return true
 }
 
 function compile(input, file) {
+    if (!Number.isInteger(input) || l <= 0) throw new Error('l의 초기값은 자연수만 됩니다.')
     file = `I l = ${input}\n${file}`
     const change = {
         '/': 'while',
